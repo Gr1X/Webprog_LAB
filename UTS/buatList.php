e5774171005e1b61523aca4f9aab2eae656422f6
KWasteds <kelvinjo2008@gmail.com>
2024-10-10 15:58:58 +0700

    buatlist.php

--- UTS/buatList.php
+++ UTS/buatList.php
@@ -2,6 +2,18 @@
 session_start();
 require_once('db.php');
 
+// 1 baris berarti menandakan 1 item yang dimiliki username dan pada judul
 $judul = $_POST['judul'];
 $username = $_SESSION['username'];
 $nama_item = $_POST['nama_item'];
+
+$en_password = password_hash($password, PASSWORD_BCRYPT);
+  
+$query4 = "INSERT INTO itemlist(username, title, nama_item)
+            VALUES(?, ?, ?)";
+
+$result1 = $db->prepare($query4);
+$result1->execute([$username, $judul, $nama_item]);
+
+
+header('location: showTask.php');
