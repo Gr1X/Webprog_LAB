1687d74bb47ee19505592f44028bad38dcdceebd
Grefred <133239181+Gr1X@users.noreply.github.com>
2024-10-10 13:04:11 +0700

    register

--- UTS/register_process.php
+++ UTS/register_process.php
@@ -1,9 +1,10 @@
 <?php
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $repassword = $_POST['repass'];
     $email = $_POST['email'];
-    $fName = $_POST['firstname'];
-    $lName = $_POST['lastname'];
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
 
     
 ?>
